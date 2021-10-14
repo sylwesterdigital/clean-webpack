8fe36de109f3464e26b7cc1dc91501d5accd38fe
Sylwester K. Mielniczuk <sylwester@mielniczuk.com>
2021-10-14 20:07:24 +0100

    clean plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -16,5 +17,6 @@ module.exports = {
       template: path.resolve(__dirname, './src/template.html'), // template file
       filename: 'index.html', // output file
     }),
+    new CleanWebpackPlugin(),
   ],  
 }
